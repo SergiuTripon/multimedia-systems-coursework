06ca290b9d65c5ba139b7ee079a7aab11ed1e1d7
SergiuTripon <contact@sergiu-tripon.com>
2016-03-21 21:45:48 +0000

    Added comments.

--- recv.c
+++ recv.c
@@ -94,26 +94,43 @@ void recv_packet(int seqno, int len, char *data, FILE *ofile, int strategy) {
 #ifdef SMOOTH
 
 	  /* Beginning of code by Sergiu Tripon */
-	  int gap = (seqno - 1) - prev_seqno;
-	  float one_sample = ((float) 1 / (gap + 1));
-	  float two_samples1 = ((float) 1 / (gap + 2));
-	  float two_samples2 = ((float) two_samples1 * 2);
 
+	  // calculate gap
+	  int gap = (seqno - 1) - prev_seqno;
+
+	  // calculate steps
+	  // if there is one sample, step is 0.5
+	  float one_sample = ((float) 1 / (gap + 1));
+	  // if there are two samples, steps are 0.3333 and 0.6667
+	  float two_samples1 = ((float) 1 / (gap + 2));
+	  float two_samples2 = ((float) 1 - two_samples1);
+
+	  // fill the gap with the last half of the previous packet, except the penultimate and last sample
 	  fwrite(prev_packet_samples + 160, 2, 158, ofile);
 
-	  int16_t last_sample = ((prev_packet_samples[319] * two_samples2) + (samples[0] * two_samples1));
-	  int16_t penultimate_sample = ((prev_packet_samples[318] * one_sample) + (last_sample * one_sample));
+	  // calculate last sample of the previous packet
+	  short int last_sample = ((prev_packet_samples[319] * two_samples2) + (samples[0] * two_samples1));
+	  // calculate penultimate sample from the previous packet, based on the last sample
+	  short int penultimate_sample = ((prev_packet_samples[318] * one_sample) + (last_sample * one_sample));
 
+	  // fill the gap with the penultimate sample of the previous packet
 	  fwrite(&penultimate_sample, 2, 1, ofile);
+	  // fill the gap with the last sample of the previous packet
 	  fwrite(&last_sample, 2, 1, ofile);
 
-	  int16_t first_sample = ((prev_packet_samples[319] * two_samples1) + (samples[0] * two_samples2));
-	  int16_t second_sample = ((first_sample * one_sample) + (samples[1] * one_sample));
+	  // calculate first sample of the current packet
+	  short int first_sample = ((prev_packet_samples[319] * two_samples1) + (samples[0] * two_samples2));
+	  // calculate second sample of the current packet, based on the first sample
+	  short int second_sample = ((first_sample * one_sample) + (samples[1] * one_sample));
 
+	  // fill the gap with the first sample of the current packet
 	  fwrite(&first_sample, 2, 1, ofile);
+	  // fill the gap with the second sample of the current packet
 	  fwrite(&second_sample, 2, 1, ofile);
 
+	  // fill the gap with the first half of the current packet, except the first and second sample
 	  fwrite(samples, 2, 158, ofile);
+
 	  /* End of code by Sergiu Tripon */
 
 #else
@@ -142,47 +159,100 @@ void recv_packet(int seqno, int len, char *data, FILE *ofile, int strategy) {
 	/* We're going to interpolate a whole new packet (or several packets) in the frequency domain. */
 
 	/* Beginning of code by Sergiu Tripon */
+
+	// calculate the gap
 	int gap = (seqno - 1) - prev_seqno;
+	// if the gap is shorter/equal than/to three packets
 	if (gap <= 3) {
+	  // calculate dct of the packet before the gap
 	  int *dct_before = dct(prev_packet_samples, num_samples);
+	  // calculate dct of the packet after the gap
 	  int *dct_after = dct(samples, num_samples);
+	  // if the gap is exactly one packet long
 	  if (gap == 1) {
+
+	    // test
 	    //printf("gap: %d\n", gap);
+
+	    // calculate the missing sequence number
 	    missing_seqno = prev_seqno + 1;
+
+	    // more tests
 	    //printf("missing_seqno: %d\n", missing_seqno);
 	    //printf("seqno: %d\n", seqno);
+
+	    // while the missing sequence number is less than the current sequence number
 	    while (missing_seqno < seqno) {
+	      // declare a new set of coefficients
 	      int new_set[320];
+	      // loop as many times as the number of samples in a packet
 	      for(int i = 0; i < 320; i++) {
+	        // test
 	        //printf("num_samples: %d\n", num_samples);
+
+	        // average the DCT coefficients and add the result to the new set of coefficients
 	        new_set[i] = (dct_before[i] + dct_after[i]) / 2;
 	      }
-	      fwrite(idct(new_set, 320), 2, 320, ofile);
+	      // calculate the IDCT of the new set of coefficients
+	      short int *idct_new_set = idct(new_set, 320);
+	      // fill the gap with the IDCT of the interpolated DCT coefficients
+	      fwrite(idct_new_set, 2, 320, ofile);
+	      // increment the missing sequence number
 	      missing_seqno++;
 	    }
+	  // if the gap is exactly two or three packets long
 	  } else if (gap == 2 || gap == 3) {
+
+	    // test
 	    //printf("gap: %d\n", gap);
+
+	    // calculate the missing sequence number
 	    missing_seqno = prev_seqno + 1;
+
+	    // more tests
 	    //printf("missing_seqno: %d\n", missing_seqno);
 	    //printf("seqno: %d\n", seqno);
+
+	    // while the missing sequence number is less than the current sequence number
 	    while (missing_seqno < seqno) {
+	      // declare a new set of coefficients
 	      int new_set[320];
+	      // calculate the weighted average before the gap
 	      double weighted_avg_before = (double) (missing_seqno - prev_seqno) / (seqno - prev_seqno);
+	      // calculate the weighted average after the gap
 	      double weighted_avg_after = (double) (seqno - missing_seqno) / (seqno - prev_seqno);
+	      // loop as many times as the number of samples in a packet
 	      for(int i = 0; i < 320; i++) {
+	        /* sum the products of the weighted averages and DCT coefficients and add the result to
+	           the new set of coefficients */
 	        new_set[i] = (weighted_avg_before * dct_before[i]) + (weighted_avg_after * dct_after[i]);
 	      }
-	      fwrite(idct(new_set, 320), 2, 320, ofile);
+	      // calculate the IDCT of the new set of coefficients
+	      short int *idct_new_set = idct(new_set, 320);
+	      // fill the gap with the IDCT of the interpolated DCT coefficients
+	      fwrite(idct_new_set, 2, 320, ofile);
+	      // increment the missing sequence number
 	      missing_seqno++;
 	    }
 	  }
+	// if the gap is longer than three packets
 	} else if (gap > 3) {
+
+	  // test
 	  //printf("gap: %d\n", gap);
+
+	  // calculate the missing sequence number
 	  missing_seqno = prev_seqno + 1;
+
+	  // more tests
 	  //printf("missing_seqno: %d\n", missing_seqno);
 	  //printf("seqno: %d\n", seqno);
+
+	  // while the missing_sequence number is less than the current sequence number
 	  while (missing_seqno < seqno) {
+	    // fill the gap with silence
 	    write_silence(ofile, 320);
+	    // increment the missing sequence number
 	    missing_seqno++;
 	  }
 	}
